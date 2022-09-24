32594f18912eb456a9684c4a0b3b172e1939cd9a
Thed <theodore1seriakov@gmail.com>
2022-09-24 15:21:27 +0600

    No Web Sockets Yet W.

--- app/src/main/java/com/example/kbway/userMap/UserMapFragment.kt
+++ app/src/main/java/com/example/kbway/userMap/UserMapFragment.kt
@@ -1,17 +1,23 @@
 package com.example.kbway.userMap
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -27,14 +33,13 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
-import com.google.android.gms.tasks.Task
 import timber.log.Timber
 
-class UserMapFragment : Fragment() {
+
+class UserMapFragment : Fragment(), LocationListener {
 
     private lateinit var client: FusedLocationProviderClient
     private lateinit var binding: UserMapBinding
-//    private lateinit var myLocation: LatLng
 
     //inflating Fragment with MapLayout
     override fun onCreateView(
@@ -55,30 +60,20 @@ class UserMapFragment : Fragment() {
             //creating map client
             client = LocationServices.getFusedLocationProviderClient(activity as AppCompatActivity)
 
-            val firstCurrLocation = getCurrentLocation()
+            getCurrentLocation()
 
-            val startCamera = CameraPosition.builder()
-                .target(LatLng(firstCurrLocation.latitude, firstCurrLocation.longitude))
-                .zoom(16f)
-                .bearing(0f)
-                .tilt(45f)
-                .build()
+            val data2 =
+                arguments?.getParcelable<AllRouteData.AllRouteDataItem?>("name")
 
-            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(startCamera), 2000, null)
-
-            mMap.addMarker(
-                MarkerOptions()
-                    .position(LatLng(firstCurrLocation.latitude, firstCurrLocation.longitude))
-                    .title("Вы находитесь здесь!")
-                    .icon(bitmapDescriptorFromVector(R.drawable.ic_user_location))
-            )
-
-            mMap.addMarker(
-                MarkerOptions()
-                    .position(LatLng(42.797655661005464, 73.85601008276385))
-                    .title("Автобус в пути!")
-                    .icon(bitmapDescriptorFromVector(R.drawable.ic_bus_lacation))
-            )
+            if (data2 != null) {
+                mMap.addMarker(
+                    MarkerOptions()
+                        .position(LatLng((data2.routeDriver?.get(0)?.latRouteDrivers as Double),
+                            (data2.routeDriver[0].lngRouteDrivers  as Double)))
+                        .title("Автобус в пути!")
+                        .icon(bitmapDescriptorFromVector(R.drawable.ic_bus_lacation))
+                )
+            }
 
             mMap.addMarker(
                 MarkerOptions()
@@ -150,34 +145,13 @@ class UserMapFragment : Fragment() {
             requireActivity().onBackPressed()
         }
 
-        binding.gpsButton.setOnClickListener {
-            binding.gpsButton.setImageResource(R.drawable.ic_gps_enabled)
-            val zoomedFragment =
-                childFragmentManager.findFragmentById(R.id.userMapFragment) as SupportMapFragment?
-            zoomedFragment!!.getMapAsync { zoomedMap ->
-                zoomedMap.mapType = GoogleMap.MAP_TYPE_NORMAL
-                val zoomCurrentLocation = getCurrentLocation()
-                val zoomedCamera = CameraPosition.builder()
-                    .target(LatLng(zoomCurrentLocation.latitude, zoomCurrentLocation.longitude))
-                    .zoom(18f)
-                    .bearing(0f)
-                    .tilt(45f)
-                    .build()
-                zoomedMap.animateCamera(
-                    CameraUpdateFactory.newCameraPosition(zoomedCamera),
-                    2000,
-                    null
-                )
-            }
-        }
-
         binding.menuImageView.setOnClickListener {
             changeFragment(SettingsFragment(), R.id.contentContainer)
         }
     }
 
     private fun setRouteColor(color: Int, double: List<List<Double>>?) {
-        double?.let { getLatLng(it) }?.let {
+        double?.let { getRouteLatLng(it) }?.let {
             PolylineOptions()
                 .addAll(it)
                 .width(25f)
@@ -194,7 +168,7 @@ class UserMapFragment : Fragment() {
         }
     }
 
-    private fun getLatLng(listOfListOfDouble: List<List<Double>>): List<LatLng> {
+    private fun getRouteLatLng(listOfListOfDouble: List<List<Double>>): List<LatLng> {
         val listOfLatLng = ArrayList<LatLng>(listOfListOfDouble.size)
         for (latLng in listOfListOfDouble) {
             listOfLatLng.add(LatLng(latLng.first(), latLng.last()))
@@ -202,48 +176,148 @@ class UserMapFragment : Fragment() {
         return listOfLatLng
     }
 
-    private fun getCurrentLocation(): LatLng {
-        var myLocation = LatLng(42.797655661005464, 78.85801008276300)
-        //Checking permission
+    private fun getCurrentLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+//                get your latitude and longitude in hear
+                if (ActivityCompat.checkSelfPermission(
+                        (context as AppCompatActivity),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        (context as AppCompatActivity),
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermission()
+                    return
+                }
+
+                client.lastLocation.addOnCompleteListener((context as AppCompatActivity)) { task ->
+                    val location: Location? = task.result
+                    Toast.makeText(
+                        context as AppCompatActivity,
+                        "${location?.longitude}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    if (location == null) {
+                        Toast.makeText(
+                            (context as AppCompatActivity),
+                            "There is null data",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    } else {
+                        val mapFragment =
+                            childFragmentManager.findFragmentById(R.id.userMapFragment) as SupportMapFragment?
+                        mapFragment!!.getMapAsync { mMap ->
+
+                            binding.gpsButton.setOnClickListener {
+                                binding.gpsButton.setImageResource(R.drawable.ic_gps_enabled)
+                                val zoomedFragment =
+                                    childFragmentManager.findFragmentById(R.id.userMapFragment) as SupportMapFragment?
+                                zoomedFragment!!.getMapAsync { zoomedMap ->
+                                    zoomedMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+                                    val zoomedCamera = CameraPosition.builder()
+                                        .target(
+                                            LatLng(
+                                                location.latitude,
+                                                location.longitude
+                                            )/*LatLng(zoomCurrentLocation.latitude, zoomCurrentLocation.longitude)*/
+                                        )
+                                        .zoom(18f)
+                                        .bearing(0f)
+                                        .tilt(45f)
+                                        .build()
+                                    zoomedMap.animateCamera(
+                                        CameraUpdateFactory.newCameraPosition(zoomedCamera),
+                                        1000,
+                                        null
+                                    )
+                                }
+                            }
+
+                            mMap.addMarker(
+                                MarkerOptions()
+                                    .position(
+                                        LatLng(
+                                            location.latitude,
+                                            location.longitude
+                                        )
+                                    )
+                                    .title("Вы находитесь здесь!")
+                                    .icon(bitmapDescriptorFromVector(R.drawable.ic_user_location))
+                            )
+                            val startCamera =
+                                LatLng(
+                                    location.latitude,
+                                    location.longitude
+                                )
+                                    ?.let {
+                                        CameraPosition.builder()
+                                            .target(it /*LatLng(firstCurrLocation.latitude, firstCurrLocation.longitude)*/)
+                                            .zoom(16f)
+                                            .bearing(0f)
+                                            .tilt(45f)
+                                            .build()
+                                    }
+                            startCamera?.let { CameraUpdateFactory.newCameraPosition(it) }
+                                ?.let { mMap.animateCamera(it, 1000, null) }
+                        }
+                    }
+                }
+            } else {
+                //settings open hear
+                Toast.makeText(
+                    (context as AppCompatActivity),
+                    "Torn on location",
+                    Toast.LENGTH_SHORT
+                ).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            //request permission hear
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            (requireActivity()),
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            PERMISSION_REQUEST_ACCESS_LOCATION
+        )
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
         if (ActivityCompat.checkSelfPermission(
-                activity as AppCompatActivity,
+                (context as AppCompatActivity),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                (context as AppCompatActivity),
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            val locationTask: Task<Location> = client.lastLocation
-            locationTask.addOnSuccessListener() { lastLocation ->
-                // on Success
-                if (lastLocation != null) {
-                    //Sync map
-                    SupportMapFragment().getMapAsync {
-                        it.isMyLocationEnabled = true
-                        val latLng = LatLng(lastLocation.latitude, lastLocation.longitude)
-                        myLocation = latLng
-                    }
-                }
-            }
-        } else {
-            ActivityCompat.requestPermissions(
-                activity as AppCompatActivity,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                44
-            )
-
-            val locationTask2: Task<Location> = client.lastLocation
-            locationTask2.addOnSuccessListener { lastLocation ->
-                // on Success
-                if (lastLocation != null) {
-                    //Sync map
-                    SupportMapFragment().getMapAsync {
-                        it.isMyLocationEnabled = true
-                        val latLng2 = LatLng(lastLocation.latitude, lastLocation.longitude)
-                        myLocation = latLng2
-                    }
-                }
-            }
+            return true
         }
-        return myLocation
+        return false
     }
+
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+    }
+
 
     private fun bitmapDescriptorFromVector(vectorResId: Int): BitmapDescriptor {
         val vectorDrawable = ContextCompat.getDrawable(requireContext(), vectorResId)
@@ -278,9 +352,39 @@ class UserMapFragment : Fragment() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if (requestCode == 44) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_ACCESS_LOCATION) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(requireActivity().applicationContext, "Granted", Toast.LENGTH_SHORT)
+                    .show()
                 getCurrentLocation()
+            } else {
+                Toast.makeText(requireActivity().applicationContext, "Denied", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    //это доп колбэк на случай если соновной не сработает
+    override fun onLocationChanged(location: Location) {
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.userMapFragment) as SupportMapFragment?
+        mapFragment!!.getMapAsync { mMap ->
+
+            binding.gpsButton.setOnClickListener {
+                binding.gpsButton.setImageResource(R.drawable.ic_gps_enabled)
+
+                mMap.addMarker(
+                    MarkerOptions()
+                        .position(
+                            LatLng(
+                                location.latitude,
+                                location.longitude
+                            )
+                        )
+                        .title("Вы находитесь здесь!")
+                        .icon(bitmapDescriptorFromVector(R.drawable.ic_user_location))
+                )
             }
         }
     }
